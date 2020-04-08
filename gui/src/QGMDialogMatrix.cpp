af94d61e512a996ed30092c5ae23ceee6c28123f
rkuehl <robert.kuehl@iwr.uni-heidelberg.de>
2020-04-08 16:08:18 +0200

    - added free input functionality for MatrixDialog - added translate functionality for MatrixDialog - added rotate functionality for MatrixDialog - added scale functionality for MatrixDialog

--- gui/src/QGMDialogMatrix.cpp
+++ gui/src/QGMDialogMatrix.cpp
@@ -1,8 +1,16 @@
+#ifdef _MSC_VER
+#define _USE_MATH_DEFINES
+#endif
+#include <cmath>
+
 #include "QGMDialogMatrix.h"
 #include "ui_QGMDialogMatrix.h"
 
 #include <QClipboard>
 #include <QDoubleValidator>
+#include <QDoubleSpinBox>
+#include <GigaMesh/logging/Logging.h>
+
 
 QGMDialogMatrix::QGMDialogMatrix(QWidget *parent) :
     QDialog(parent),
@@ -31,10 +39,40 @@ QGMDialogMatrix::QGMDialogMatrix(QWidget *parent) :
 	mLineEditPtrs[14] = ui->lineEdit_mat23;
 	mLineEditPtrs[15] = ui->lineEdit_mat33;
 
+	uint8_t index = 0;
 	for(auto lineEditPtr : mLineEditPtrs)
 	{
 		lineEditPtr->setValidator(mValidator.get());
+		lineEditPtr->setProperty("index", index++);
 	}
+
+	//tabwidget events
+	connect(ui->tabWidget, &QTabWidget::currentChanged, this, &QGMDialogMatrix::tabChanged);
+	tabChanged(ui->tabWidget->currentIndex());
+
+	//free edit widgets
+	connect(ui->pushButton_CopyToClipboard, &QPushButton::clicked, this, &QGMDialogMatrix::copyToClipboard);
+	connect(ui->pushButton_loadFromClipboard, &QPushButton::clicked, this, &QGMDialogMatrix::fetchClipboard);
+
+	//translate
+	connect(ui->doubleSpinBox_transX, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateTranslate);
+	connect(ui->doubleSpinBox_transY, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateTranslate);
+	connect(ui->doubleSpinBox_transZ, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateTranslate);
+
+	//scale
+	connect(ui->doubleSpinBox_scaleX, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateScale);
+	connect(ui->doubleSpinBox_scaleY, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateScale);
+	connect(ui->doubleSpinBox_scaleZ, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateScale);
+
+	//rotate
+	ui->buttonGroup_axisSel->setId(ui->radioButton_axisX   , 0);
+	ui->buttonGroup_axisSel->setId(ui->radioButton_axisY   , 1);
+	ui->buttonGroup_axisSel->setId(ui->radioButton_axisZ   , 2);
+	ui->buttonGroup_axisSel->setId(ui->radioButton_axisFree, 3);
+	connect(ui->doubleSpinBox_angle, QOverload<double>::of(&QDoubleSpinBox::valueChanged), this, &QGMDialogMatrix::updateRotate);
+	connect(ui->horizontalSlider_angle, &QSlider::valueChanged, this, &QGMDialogMatrix::updateRotateBySlider);
+
+	updateMatrixValues();
 }
 
 QGMDialogMatrix::~QGMDialogMatrix()
@@ -44,31 +82,36 @@ QGMDialogMatrix::~QGMDialogMatrix()
 
 void QGMDialogMatrix::fetchClipboard()
 {
-	QClipboard* clipboard = QApplication::clipboard();
-	QString clipBoardStr = clipboard->text( QClipboard::Clipboard );
+	QString clipBoardStr = QApplication::clipboard()->text( QClipboard::Clipboard );
 
-	QStringList values = clipBoardStr.simplified().split( " ", QString::SkipEmptyParts);
+	const QStringList values = clipBoardStr.simplified().split( " ", QString::SkipEmptyParts);
 
 	if(values.size() != 16)
 	{
 		return;
 	}
 
-	auto matIt = mMatrixData.begin();
+	std::array<double,16> tempVals;
+
+	auto matIt = tempVals.begin();
 	for(const auto& value : values)
 	{
 		bool ok = false;
-		auto val = value.toDouble(&ok);
+		const auto val = value.toDouble(&ok);
 
-		if(ok)
+		if(!ok)
 		{
-			(*matIt) = val;
+			return;
 		}
-		++matIt;
+
+		(*matIt++) = val;
 	}
+
+	mMatrixData = std::move(tempVals);
 
 	updateMatrixValues();
 }
+
 
 void QGMDialogMatrix::getValues(std::vector<double>& values) const
 {
@@ -77,8 +120,7 @@ void QGMDialogMatrix::getValues(std::vector<double>& values) const
 
 	for(auto& value : values)
 	{
-		value = (*it)->text().toDouble();
-		++it;
+		value = (*it++)->text().toDouble();
 	}
 }
 
@@ -88,7 +130,149 @@ void QGMDialogMatrix::updateMatrixValues()
 
 	for(auto value : mMatrixData)
 	{
-		(*it)->setText(QString::number(value));
-		++it;
+		(*it++)->setText(QString::number(value));
 	}
+}
+
+void QGMDialogMatrix::tabChanged(int index)
+{
+	bool enable = index == 0;
+
+	for(auto matrixField : mLineEditPtrs)
+	{
+		matrixField->setEnabled(enable);
+	}
+}
+
+void QGMDialogMatrix::updateScale(double value)
+{
+	if(ui->checkBox_scaleUniform->isChecked())
+	{
+		ui->doubleSpinBox_scaleX->blockSignals(true);
+		ui->doubleSpinBox_scaleY->blockSignals(true);
+		ui->doubleSpinBox_scaleZ->blockSignals(true);
+
+		ui->doubleSpinBox_scaleX->setValue(value);
+		ui->doubleSpinBox_scaleY->setValue(value);
+		ui->doubleSpinBox_scaleZ->setValue(value);
+
+		ui->doubleSpinBox_scaleX->blockSignals(false);
+		ui->doubleSpinBox_scaleY->blockSignals(false);
+		ui->doubleSpinBox_scaleZ->blockSignals(false);
+	}
+
+	const double scaleX = ui->doubleSpinBox_scaleX->value();
+	const double scaleY = ui->doubleSpinBox_scaleY->value();
+	const double scaleZ = ui->doubleSpinBox_scaleZ->value();
+
+	mMatrixData = {scaleX,    0.0,    0.0, 0.0,
+	                  0.0, scaleY,    0.0, 0.0,
+	                  0.0,    0.0, scaleZ, 0.0,
+	                  0.0,    0.0,    0.0, 1.0};
+
+	updateMatrixValues();
+}
+
+void QGMDialogMatrix::updateTranslate()
+{
+	const double xVal = ui->doubleSpinBox_transX->value();
+	const double yVal = ui->doubleSpinBox_transY->value();
+	const double zVal = ui->doubleSpinBox_transZ->value();
+
+	mMatrixData = {1.0 ,0.0 ,0.0 ,0.0,
+	               0.0 ,1.0 ,0.0 ,0.0,
+	               0.0 ,0.0 ,1.0 ,0.0,
+	               xVal,yVal,zVal,1.0};
+
+	updateMatrixValues();
+}
+
+std::array<double,16> getAngleAxisMat(double aX, double aY, double aZ, double s, double c)
+{
+	const double len = std::sqrt(aX * aX + aY * aY + aZ * aZ);
+	aX /= len;
+	aY /= len;
+	aZ /= len;
+
+	const double mC = (1.0 - c);
+
+	return {     c + aX * aX * mC,     aY*aX*mC + aZ * s,     aZ*aX*mC - aY * s, 0.0,
+		    aX * aY * mC - aZ * s,      c + aY * aY * mC, aZ * aY * mC + aX * s, 0.0,
+		    aX * aZ * mC + aY * s, aY * aZ * mC - aX * s,      c + aZ * aZ * mC, 0.0,
+		    0.0, 0.0, 0.0, 1.0};
+}
+
+void QGMDialogMatrix::updateRotate(double angle)
+{
+	//update slider
+	ui->horizontalSlider_angle->blockSignals(true);
+	const double sliderMax = static_cast<double>(ui->horizontalSlider_angle->maximum());
+	const double sliderMin = static_cast<double>(ui->horizontalSlider_angle->minimum());
+	const double percent = (angle - ui->doubleSpinBox_angle->minimum()) / (ui->doubleSpinBox_angle->maximum() - ui->doubleSpinBox_angle->minimum());
+
+	ui->horizontalSlider_angle->setValue(percent * (sliderMax - sliderMin) + sliderMin);
+	ui->horizontalSlider_angle->blockSignals(false);
+
+	const double s = sin(angle * M_PI / 180.0);
+	const double c = cos(angle * M_PI / 180.0);
+
+	switch (ui->buttonGroup_axisSel->checkedId()) {
+		case 0: // x-axis
+			mMatrixData = {1.0, 0.0, 0.0, 0.0,
+			               0.0,   c,   s, 0.0,
+			               0.0,  -s,   c, 0.0,
+			               0.0, 0.0, 0.0, 1.0};
+			break;
+		case 1: // y-axis
+			mMatrixData = {  c, 0.0,  -s, 0.0,
+			               0.0, 1.0, 0.0, 0.0,
+			                 s, 0.0,   c, 0.0,
+			               0.0, 0.0, 0.0, 1.0};
+			break;
+		case 2: // z-axis
+			mMatrixData = {  c,   s, 0.0, 0.0,
+			                -s,   c, 0.0, 0.0,
+			               0.0, 0.0, 1.0, 0.0,
+			               0.0, 0.0, 0.0, 1.0};
+			break;
+		case 3: // free-axis
+			//avoid case, where all axis values are 0.0
+			if(std::abs(ui->doubleSpinBox_axisX->value()) <= std::numeric_limits<double>::epsilon() &&
+			   std::abs(ui->doubleSpinBox_axisY->value()) <= std::numeric_limits<double>::epsilon() &&
+			   std::abs(ui->doubleSpinBox_axisZ->value()) <= std::numeric_limits<double>::epsilon())
+			{
+				return;
+			}
+			mMatrixData = getAngleAxisMat(ui->doubleSpinBox_axisX->value(),
+			                              ui->doubleSpinBox_axisY->value(),
+			                              ui->doubleSpinBox_axisZ->value(),
+			                              s,c);
+			break;
+	}
+
+	updateMatrixValues();
+}
+
+void QGMDialogMatrix::updateRotateBySlider(int value)
+{
+	const double angleMin = ui->doubleSpinBox_angle->minimum();
+	const double angleMax = ui->doubleSpinBox_angle->maximum();
+	const double angleRange = angleMax - angleMin;
+
+	const double valRel     = static_cast<double>(value - ui->horizontalSlider_angle->minimum());
+	const double valueRange = static_cast<double>(ui->horizontalSlider_angle->maximum() - ui->horizontalSlider_angle->minimum());
+
+	ui->doubleSpinBox_angle->setValue( (valRel / valueRange * angleRange) + angleMin );
+}
+
+void QGMDialogMatrix::copyToClipboard() const
+{
+	QString clipBoardText;
+
+	for(auto value : mMatrixData)
+	{
+		clipBoardText += QString("%1 ").arg(value);
+	}
+
+	QApplication::clipboard()->setText(clipBoardText);
 }
--- gui/src/QGMDialogMatrix.h
+++ gui/src/QGMDialogMatrix.h
@@ -19,19 +19,30 @@ class QGMDialogMatrix : public QDialog
 		explicit QGMDialogMatrix(QWidget *parent = nullptr);
 		~QGMDialogMatrix();
 
-		void fetchClipboard( );
-
 		void getValues(std::vector<double>& values) const;
+
+	public slots:
+		void copyToClipboard() const;
+		void fetchClipboard( );
 
 	private:
 		Ui::QGMDialogMatrix *ui;
 
-		std::array<double,16> mMatrixData;
+		//mMatrixData stores the matrix in column-major order
+		std::array<double,16> mMatrixData = {1.0,0.0,0.0,0.0,
+		                                     0.0,1.0,0.0,0.0,
+		                                     0.0,0.0,1.0,0.0,
+		                                     0.0,0.0,0.0,1.0};
 		std::array<QLineEdit*,16> mLineEditPtrs;
 
 		std::unique_ptr<QDoubleValidator> mValidator;
 	private slots:
 		void updateMatrixValues();
+		void tabChanged(int index);
+		void updateScale(double value);
+		void updateTranslate();
+		void updateRotate(double angle);
+		void updateRotateBySlider(int value);
 };
 
 #endif // QGMDIALOGMATRIX_H
